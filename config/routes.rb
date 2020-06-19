f2b431f4419e8077a5c8d5049f0bd32c28e07c00
ricardo39985 <ricardo39985@gmail.com>
2020-06-18 21:12:21 -0400

    Add resources for listings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :listings
   devise_for :users
   get '/', to: 'application#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
