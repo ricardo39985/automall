030f2ca6cf2cfa48e4f9c0e11792946170b9dd66
ricardo39985 <ricardo39985@gmail.com>
2020-06-18 21:16:06 -0400

    Add Listings controller

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class ListingsController < ApplicationController
+  def index; end
+def show
+  @listing= Listing.find(params[:id])
+  
+end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to @listing
+      
+    else
+      # byebug
+      render 'new'
+    end
+  end
+
+  def update; end
+
+  def delete; end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:year, :make, :model, :color, :features, :miles)
+  end
+end
