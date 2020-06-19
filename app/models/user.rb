b4a0e02b4ab25a6bd90826a92d9b98174061baa1
ricardo39985 <ricardo39985@gmail.com>
2020-06-18 21:11:44 -0400

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :photo
+  validates :email, presence: true
+  validates :email, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
