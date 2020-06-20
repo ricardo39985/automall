418d021a35980b8750f1c83e61ace08876b33733
ricardo39985 <ricardo39985@gmail.com>
2020-06-19 21:06:05 -0400

    Add bootstrap and dependancies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,14 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import "bootstrap"
+import "../stylesheets/application"
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/alert';
 
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +22,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+    $('.toast').toast({ autohide: false })
+    $('.toast').toast('show')
+  })
